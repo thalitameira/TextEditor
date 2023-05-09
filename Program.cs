09737dcceb953f09cb18326135aebc0d352051e1
thalitameira <117841322+thalitameira@users.noreply.github.com>
2023-05-09 20:20:09 -0300

    implementa método Open

--- Program.cs
+++ Program.cs
@@ -30,7 +30,23 @@ namespace TextEditor
             }
         }
 
-        static void Open() { }
+        static void Open()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+
+        }
 
         static void Edit()
         {
